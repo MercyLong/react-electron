a19c30c1f0d3573165455e0d44fa9a2e033e806b
jacky.lx <jacky.lx@alibaba-inc.com>
2019-02-14 17:13:28 +0800

    支持REACT打包

--- src/render/index.js
+++ src/render/index.js
@@ -1,1 +1,5 @@
-console.log("21312");
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+import "./index.less";
+ReactDom.render(<App />, document.getElementById("app"));
